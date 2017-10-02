87c7291fd4bf7140b31628a6661d541f218192e2
Manishasarvi <msarvi2806@gmail.com>
2017-10-02 01:17:31 -0300

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+Class Student
+{
+int id;
+String name;
+public static void main(String args[])
+{
+Student s=new Student();
+System.out.println(s.id); //accessing member through reference variable
+System.out.println(s.name);
+}
+}
